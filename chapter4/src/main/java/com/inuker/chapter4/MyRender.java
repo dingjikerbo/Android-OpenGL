c90e081f497640f884cb8e7c58aa41e4e10955c6
liwentian <wentian.li@hobot.cc>
2017-07-24 11:29:25 +0800

    chapter4

---
+++ chapter4/src/main/java/com/inuker/chapter4/MyRender.java
@@ -0,0 +1,112 @@
+package com.inuker.chapter4;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+import com.inuker.library.ShaderHelper;
+import com.inuker.library.TextResourceReader;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
+import static android.opengl.GLES20.GL_TRIANGLE_STRIP;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glLineWidth;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glViewport;
+
+/**
+ * Created by liwentian on 17/7/24.
+ */
+
+public class MyRender implements GLSurfaceView.Renderer {
+
+    private static final int POSITION_COMPONENT_COUNT = 2;
+
+    private static final int COLOR_COMPONENT_COUNT = 3;
+
+    private static final int BYTES_PER_FLOAT = 4;
+
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+
+    private Context mContext;
+
+    private final FloatBuffer vertexData;
+
+    private static final float[] tableVerticesWithTriangles = {
+            0f, 0f, 1f, 1f, 1f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+    };
+
+    private int aPositionLocation;
+
+    private int aColorLocation;
+
+    public MyRender(Context context) {
+        mContext = context;
+
+        vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(tableVerticesWithTriangles);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        String vertexShaderSrc = TextResourceReader.readTextFileFromResource(mContext, R.raw.vertex);
+        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSrc);
+
+        String fragmentShaderSrc = TextResourceReader.readTextFileFromResource(mContext, R.raw.fragment);
+        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSrc);
+
+        int program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+
+        glUseProgram(program);
+
+        aPositionLocation = glGetAttribLocation(program, "a_Position");
+        aColorLocation = glGetAttribLocation(program, "a_Color");
+
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aColorLocation);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glClearColor(0f, 0f, 0f, 1f);
+
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
+    }
+}
